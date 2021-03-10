231119219aaec249022ec6d04671750d703eb6de
Monica <monica22valverde@gmail.com>
2021-03-10 19:17:51 +0100

    Mover Nave Horizontalmente

---
+++ Valverde_Monica_ExamGlobal/Assets/Scripts/Camara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    [SerializeField] Transform PointAt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(PointAt);
+    }
+}
---
+++ Valverde_Monica_ExamGlobal/Assets/Scripts/MoverCaja.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCaja : MonoBehaviour
+{
+
+    [SerializeField] float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //declaro la funcion para que se mueva la caja
+        MoverCajita();
+
+    }
+
+   
+    
+   void MoverCajita()
+
+    {
+        //Llamo a la funcion que mueve la caja
+        //Mueve la caja en horizontal (pero al reves no se por que xD)
+        float desplX = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(Vector3.left * desplX * Time.deltaTime);
+
+        //Limitador en X
+        if (transform.position.x <= -185f)
+        {
+            transform.position = new Vector3(-185f, transform.position.y);
+        }
+        else if (transform.position.x >= 185f)
+        {
+            transform.position = new Vector3(185f, transform.position.y);
+        }
+
+       // float desplZ = Input.GetAxis()
+
+
+
+    }
+}
+
